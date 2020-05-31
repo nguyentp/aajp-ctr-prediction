74b764524313d3fcd8778d9c3a9a17a1f4971d5f
nguyentp <nguyentp2788@gmail.com>
2020-05-31 12:18:32 +0900

    Remove movielens process

--- ajctr/features/make_features.py
+++ ajctr/features/make_features.py
@@ -1,9 +1,8 @@
 # -*- coding: utf-8 -*-
 from ajctr.helpers import timing
-from ajctr.features import make_movielens, make_avazu
+from ajctr.features import make_avazu
 
 
 @timing
 def make(is_debug=False):
-    make_movielens.make(is_debug)
     make_avazu.make(is_debug)
